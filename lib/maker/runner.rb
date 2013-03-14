834316c0c498fb3adb7bf116d84c440adab2b1ac
Steve Loveless <steve.loveless@gmail.com>
2013-03-14 15:55:01 -0700

    Refactored out failure methods

--- lib/maker/runner.rb
+++ lib/maker/runner.rb
@@ -33,6 +33,44 @@ class Maker
       @user = user
     end
 
+    def ssh_options
+      options = {
+        user: @user,
+        #  verbose: :debug
+      }
+
+      options.merge!(keys: ssh_key_path) if ssh_key_path
+
+      options
+    end
+
+    def maker_failure(exception, start_time)
+      error = <<-ERROR
+*** Maker Error! ***
+* Exception: #{exception.wrapped}
+* Plan Duration: #{exception.result.finish_at - exception.result.start_at}
+* Total Duration: #{exception.result.finish_at - start_time}
+* SCP source: #{exception.result.opts[:scp_src]}
+* SCP destination: #{exception.result.opts[:scp_dst]}
+* STDERR: #{exception.result.stderr}
+      ERROR
+
+      abort(error.red)
+    end
+
+    def plan_failure(output, start_time)
+      error = <<-ERROR
+*** Maker Error! ***
+* Plan failed: #{output.cmd}
+* Exit code: #{output.exit_code}
+* Plan Duration: #{output.finish_at - output.start_at}
+* Total Duration: #{output.finish_at - start_time}
+* STDERR: #{output.stderr}
+      ERROR
+
+      abort(error.red)
+    end
+
     def run_commands
       abort('Must use Ruby 2.0.0 or greater with maker.') if RUBY_VERSION < '2.0.0'
       start_time = Time.now
@@ -41,27 +79,15 @@ class Maker
         @commands.each do |cmd|
           puts "executing command: '#{cmd}' as user #{@user} on #{@hostname}".blue
 
-          options = {
-            user: @user,
-          #  verbose: :debug
-          }
+          begin
+            r = ssh(@hostname, cmd, ssh_options)
+          rescue Net::SSH::Simple::Error => ex
 
-          options.merge!(keys: ssh_key_path) if ssh_key_path
-          r = ssh(@hostname, cmd, options)
+          end
 
           if r.exit_code.zero?
             puts "Maker finished: '#{r.cmd}'".green
           else
-            error = <<-ERROR
-*** Maker Error! ***
-Plan failed: #{r.cmd}
-Exit code: #{r.exit_code}
-Plan Duration: #{r.finish_at - r.start_at}
-Total Duration: #{r.finish_at - start_time}
-STDERR: #{r.stderr}
-            ERROR
-
-            abort(error.red)
           end
         end
       end
