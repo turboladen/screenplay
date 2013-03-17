c8d6160888bf8bdef2e3362fc624d1026e60f921
Steve Loveless <steve.loveless@gmail.com>
2013-03-17 16:49:12 -0700

    Added link action

---
+++ lib/drama/actions/link.rb
@@ -0,0 +1,37 @@
+require_relative '../action'
+
+
+class Drama
+  module Actions
+    class Link < Drama::Action
+      def initialize(
+        source: source,
+        target: target,
+        state: :exists,
+        symbolic: true
+      )
+        command = case state
+        when :absent then "rm -rf #{source}"
+        when :exists then file_exists?(source)
+        else raise "Unknown state: #{state}"
+        end
+
+        super(command)
+      end
+
+      def act(ssh, host)
+        outcome = super(ssh, host)
+        return outcome if outcome.exception?
+
+        outcome.status = case outcome.ssh_output.exit_code
+        when 0
+          :updated
+        else
+          :failed
+        end
+
+        outcome
+      end
+    end
+  end
+end
