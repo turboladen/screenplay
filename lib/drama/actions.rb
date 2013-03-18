874b198ffb641022e13b5820d5074c3bbd7f977c
Steve Loveless <steve.loveless@gmail.com>
2013-03-17 17:00:13 -0700

    Made Drama actions use their class name

--- lib/drama/actions.rb
+++ lib/drama/actions.rb
@@ -7,7 +7,7 @@ class Drama
   # Include this into any class to gain access to the commands.
   module Actions
     Drama::Actions.constants.each do |action_class|
-      define_method action_class.to_s.downcase.to_sym do |**options|
+      define_method action_class do |**options|
         klass = Drama::Actions.const_get(action_class)
         @actions << klass.new(**options)
       end
