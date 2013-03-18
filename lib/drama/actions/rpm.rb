10219e496dd89c9b16f234eb7265b66c9c0125da
Steve Loveless <steve.loveless@gmail.com>
2013-03-17 22:51:41 -0700

    Added Rpm action

---
+++ lib/drama/actions/rpm.rb
@@ -0,0 +1,44 @@
+require_relative '../action'
+
+
+class Drama
+  module Actions
+    class Rpm < Drama::Action
+      def initialize(
+        package: package,
+        state: :installed,
+        sudo: false
+      )
+        action = case state
+        when :latest then '-Uvh'
+        when :installed then '-Uvh'
+        when :removed then '-e'
+        end
+
+        command = ''
+        command << 'sudo ' if sudo
+        command << "rpm #{action} #{package}"
+
+        super(command)
+      end
+
+      def act(ssh, host)
+        outcome = super(ssh, host)
+        return outcome if outcome.error?
+
+        outcome.status = case outcome.ssh_output.exit_code
+        when 0
+          if outcome.ssh_output.stdout.match /already installed and version/m
+            :no_change
+          else
+            :updated
+          end
+        else
+          :failed
+        end
+
+        outcome
+      end
+    end
+  end
+end
