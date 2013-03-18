df69803ee3f2b886130f88305e6c0bd98620e295
Steve Loveless <steve.loveless@gmail.com>
2013-03-17 22:50:33 -0700

    File can now get remote files (like wget)

--- lib/drama/actions/file.rb
+++ lib/drama/actions/file.rb
@@ -1,4 +1,6 @@
 require_relative '../action'
+require 'open-uri'
+require 'tempfile'
 
 
 class Drama
@@ -6,11 +8,15 @@ class Drama
     class File < Drama::Action
       def initialize(
         path: path,
-        state: :exists
+        state: :exists,
+        source: nil
       )
+        @source = source
+        @path = path
+
         command = case state
-        when :absent then "rm -rf #{path}"
-        when :exists then file_exists?(path)
+        when :absent then "rm -rf #{@path}"
+        when :exists then file_exists?(@path)
         else raise "Unknown state: #{state}"
         end
 
@@ -18,6 +24,19 @@ class Drama
       end
 
       def act(ssh, host)
+        if @source
+          source_file = if ::File.exists?(@source)
+            @source
+          else
+            f = Tempfile.new('drama')
+            f.write(open(@source).read)
+            f
+          end
+
+          log ""
+          ssh.scp_ul(host, source_file, @path)
+        end
+
         outcome = super(ssh, host)
         return outcome if outcome.error?
 
