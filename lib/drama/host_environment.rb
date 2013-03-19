6b09ff06fbd32e916bdfa66f998f06359fd16e29
Steve Loveless <steve.loveless@gmail.com>
2013-03-18 23:09:26 -0700

    Ability to get OS distro and version

--- lib/drama/host_environment.rb
+++ lib/drama/host_environment.rb
@@ -8,22 +8,44 @@ class Drama
     include LogSwitch::Mixin
 
     UNAME_METHODS = %i[operating_system kernel_version architecture]
+    DISTRIBUTION_METHODS = %i[distribution distribution_version]
 
     def initialize(ssh, ssh_hostname)
       @ssh = ssh
       @ssh_hostname = ssh_hostname
+
+      @operating_system = nil
+      @kernel_version = nil
+      @architecture = nil
+
+      @distribution = nil
+      @distribution_version = nil
     end
 
     def method_missing(meth, *args)
-      super unless UNAME_METHODS.include? meth
+      if UNAME_METHODS.include? meth
+        unless @operating_system && @kernel_version && @architecture
+          command = 'uname -a'
+          result = @ssh.ssh(@ssh_hostname, command)
+          extract_os(result)
+        end
+      elsif DISTRIBUTION_METHODS.include? meth
+        unless @distribution && @distribution_version
+          command = case operating_system
+          when :linux
+            'lsb_release --description'
+          when :darwin
+            'sw_vers'
+          end
 
-      unless @operating_system && @kernel_version && @architecture
-        command = 'uname -a'
-        result = @ssh.ssh(@ssh_hostname, command)
-        extract_os(result)
+          result = @ssh.ssh(@ssh_hostname, command)
+          extract_distribution(result)
+        end
+      else
+        super
       end
 
-      instance_variable_get("@#{meth}".to_sym)
+      return instance_variable_get("@#{meth}".to_sym)
     end
 
     private
@@ -34,15 +56,30 @@ class Drama
       %r[^(?<os>[a-zA-Z]+) (?<uname>.*)] =~ ssh_result.stdout
       @operating_system = os.downcase.to_sym
 
-      if @operating_system == :darwin
+      case @operating_system
+      when :darwin
         %r[Kernel Version (?<version>\d\d\.\d\d?\.\d\d?).*RELEASE_(?<arch>\S+)] =~ uname
-        @kernel_version = version
-        @architecture = arch.downcase.to_sym
-      elsif @operating_system == :linux
+      when :linux
         %r[\S+\s\S+(?<version>\S+).*(?<arch>\S+) GNU/Linux] =~ uname
-        @kernel_version = version
-        @architecture = arch.downcase.to_sym
       end
+
+      @kernel_version = version
+      @architecture = arch.downcase.to_sym
+    end
+
+    def extract_distribution(ssh_result)
+      log "STDOUT: #{ssh_result.stdout}"
+
+      case @operating_system
+      when :darwin
+        %r[ProductVersion:\s+(?<version>\S+)] =~ ssh_result.stdout
+        distro = :osx
+      when :linux
+        %r[^Description:\s+(?<distro>[a-zA-Z]+)\s+release(?<version>\S+)] =~ ssh_result.stdout
+      end
+
+      @distribution = distro.downcase.to_sym
+      @distribution_version = version
     end
   end
 end
