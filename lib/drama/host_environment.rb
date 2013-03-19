b92c4dddc6c61b71a737900c504a2aa792ea7280
Steve Loveless <steve.loveless@gmail.com>
2013-03-19 16:16:53 -0700

    Use #define_method instead of #method_missing
    Also, use Environment info instead of a passed in ssh.

--- lib/drama/host_environment.rb
+++ lib/drama/host_environment.rb
@@ -1,14 +1,45 @@
 require_relative 'logger'
+require_relative 'environment'
 
 
 class Drama
-  attr_reader :hostname
+  attr_reader :ssh_hostname
 
   class HostEnvironment
     include LogSwitch::Mixin
 
     UNAME_METHODS = %i[operating_system kernel_version architecture]
     DISTRIBUTION_METHODS = %i[distribution distribution_version]
+
+    UNAME_METHODS.each do |meth|
+      define_method(meth) do
+        unless @operating_system && @kernel_version && @architecture
+          command = 'uname -a'
+          outcome = Drama::Environment.hosts[@ssh_hostname].ssh.run(command)
+          extract_os(outcome)
+        end
+
+        instance_variable_get("@#{meth}".to_sym)
+      end
+    end
+
+    DISTRIBUTION_METHODS.each do |meth|
+      define_method(meth) do
+        unless @distribution && @distribution_version
+          command = case self.operating_system
+          when :linux
+            'lsb_release --description'
+          when :darwin
+            'sw_vers'
+          end
+
+          outcome = Drama::Environment.hosts[@ssh_hostname].ssh.run(command)
+          extract_distribution(outcome)
+        end
+
+        instance_variable_get("@#{meth}".to_sym)
+      end
+    end
 
     def initialize(ssh, ssh_hostname)
       @ssh = ssh
@@ -24,48 +55,22 @@ class Drama
       @shell = nil
     end
 
-    def method_missing(meth, *args)
-      if UNAME_METHODS.include? meth
-        unless @operating_system && @kernel_version && @architecture
-          command = 'uname -a'
-          result = @ssh.ssh(@ssh_hostname, command)
-          extract_os(result)
-        end
-      elsif DISTRIBUTION_METHODS.include? meth
-        unless @distribution && @distribution_version
-          command = case operating_system
-          when :linux
-            'lsb_release --description'
-          when :darwin
-            'sw_vers'
-          end
-
-          result = @ssh.ssh(@ssh_hostname, command)
-          extract_distribution(result)
-        end
-      else
-        super
-      end
-
-      instance_variable_get("@#{meth}".to_sym)
-    end
-
     def shell
       return @shell if @shell
 
       command = 'echo $SHELL'
-      result = @ssh.ssh(@ssh_hostname, command)
-      log "STDOUT: #{result.stdout}"
-      %r[(?<shell>[a-z]+)$] =~ result.stdout
+      outcome = Drama::Environment.hosts[@ssh_hostname].ssh.run(command)
+      log "STDOUT: #{outcome.ssh_output.stdout}"
+      %r[(?<shell>[a-z]+)$] =~ outcome.ssh_output.stdout
       @shell = shell.to_sym
     end
 
     private
 
-    def extract_os(ssh_result)
-      log "STDOUT: #{ssh_result.stdout}"
+    def extract_os(outcome)
+      log "STDOUT: #{outcome.ssh_output.stdout}"
 
-      %r[^(?<os>[a-zA-Z]+) (?<uname>.*)] =~ ssh_result.stdout
+      %r[^(?<os>[a-zA-Z]+) (?<uname>.*)] =~ outcome.ssh_output.stdout
       @operating_system = os.downcase.to_sym
 
       case @operating_system
@@ -79,15 +84,15 @@ class Drama
       @architecture = arch.downcase.to_sym
     end
 
-    def extract_distribution(ssh_result)
-      log "STDOUT: #{ssh_result.stdout}"
+    def extract_distribution(outcome)
+      log "STDOUT: #{outcome.ssh_output.stdout}"
 
       case @operating_system
       when :darwin
-        %r[ProductVersion:\s+(?<version>\S+)] =~ ssh_result.stdout
+        %r[ProductVersion:\s+(?<version>\S+)] =~ outcome.ssh_output.stdout
         distro = :osx
       when :linux
-        %r[^Description:\s+(?<distro>[a-zA-Z]+)\s+release(?<version>\S+)] =~ ssh_result.stdout
+        %r[Description:\s+(?<distro>\w+)\s+release\s+(?<version>\S+)] =~ outcome.ssh_output.stdout
       end
 
       @distribution = distro.downcase.to_sym
