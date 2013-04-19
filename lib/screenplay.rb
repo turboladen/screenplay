004fbd9ea397a7f9943f7e4f0b71ce3e945a8af2
Steve Loveless <steve.loveless@gmail.com>
2013-04-19 13:49:22 -0700

    No more stop_on_fail for now

--- lib/screenplay.rb
+++ lib/screenplay.rb
@@ -1,11 +1,12 @@
 require 'rosh'
 require_relative 'screenplay/host'
+require_relative 'screenplay/part'
 
 
 class Screenplay
-  def self.sketch(hosts, stop_on_fail: true, on_fail: nil,
+  def self.sketch(hosts, on_fail: nil,
     report_file: nil, &block)
-    sketcher = new(hosts, stop_on_fail, on_fail)
+    sketcher = new(hosts, on_fail)
 
     starting_dir = Dir.pwd
     sketcher.sketch(&block)
@@ -29,14 +30,16 @@ class Screenplay
 
   attr_reader :hosts
 
-  def initialize(hosts, stop_on_fail, on_fail)
+  def initialize(hosts, on_fail)
     @hosts = []
     @on_fail_block = on_fail
 
     rosh = Rosh.new
 
     hosts.each do |hostname, options|
-      rosh.add_host(hostname, throw_on_fail: stop_on_fail, **options)
+      host_alias = options.delete(:alias)
+
+      rosh.add_host(hostname, host_alias: host_alias, **options)
     end
 
     rosh.hosts.each do |hostname, host|
@@ -47,11 +50,7 @@ class Screenplay
 
   def sketch
     @hosts.each do |host|
-      failure = catch(:shell_failure) do
-        yield host
-      end
-
-      puts "Stopped execution because of: #{failure}"
+      yield host
     end
   end
 end
